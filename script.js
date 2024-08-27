125919f34f1bf93dacdf4485b980eec9622c0984
dongjun-716 <djk0716@naver.com>
2024-08-27 13:20:14 +0900

    js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ document.querySelectorAll(".head_menu a").forEach((link) => {
 document.querySelector(".header").addEventListener("mouseleave", function () {
   this.style.height = "80px";
 });
+
+document.querySelectorAll(".head_menu a").forEach((a) => {
+  a.addEventListener("mouseenter", function () {
+    const bgEffect = document.querySelector(".background-effect");
+
+    bgEffect.style.width = `${this.offsetWidth}px`;
+    bgEffect.style.left = `${this.getBoundingClientRect().left}px`;
+    bgEffect.style.backgroundColor = "#fbff00";
+  });
+});
