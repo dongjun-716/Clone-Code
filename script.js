045ece892881c88ecd98a4ecc3b1e7495f63f84b
dongjun-716 <djk0716@naver.com>
2024-08-27 11:03:59 +0900

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".head_menu a").forEach((link) => {
+  link.addEventListener("mouseenter", function () {
+    document.querySelector(".header").style.height = "300px";
+  });
+});
+
+document.querySelector(".header").addEventListener("mouseleave", function () {
+  this.style.height = "80px";
+});
