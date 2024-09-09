0e72bd70213a155ed955dd495693996435c58735
dongjun-716 <djk0716@naver.com>
2024-09-09 13:18:42 +0900

    0909

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//헤더 크기 조절
+// 헤더 크기 조절
 document.querySelectorAll(".head_menu a").forEach((link) => {
   link.addEventListener("mouseenter", function () {
     document.querySelector(".header").style.height = "300px";
@@ -23,31 +23,34 @@ document.querySelectorAll(".head_menu a").forEach((a) => {
   a.addEventListener("mouseleave", function () {
     const bgEffect = document.querySelector(".background-effect");
     const header = document.querySelector(".header");
-    
+    const main = document.querySelector(".main");
+
     bgEffect.style.width = "0px";
-    header.style.backgroundColor = "";
+    if (main.getBoundingClientRect().top >= -900) {
+      header.style.backgroundColor = "";
+    }
   });
 });
 
 // Main Section 이미지 슬라이드
-const slider = document.querySelector('.slider');
+const slider = document.querySelector(".slider");
 let isDragging = false;
 let startPos = 0;
 let currentTranslate = 0;
-let prevTranslate = 0; 
+let prevTranslate = 0;
 let animationID = 0;
-let currentIndex = 0; 
+let currentIndex = 0;
 
-slider.addEventListener('mousedown', startDrag);
-slider.addEventListener('mouseup', endDrag);
-slider.addEventListener('mousemove', drag);
-slider.addEventListener('mouseleave', endDrag);
+slider.addEventListener("mousedown", startDrag);
+slider.addEventListener("mouseup", endDrag);
+slider.addEventListener("mousemove", drag);
+slider.addEventListener("mouseleave", endDrag);
 
 function startDrag(event) {
   event.preventDefault(); // 기본 드래그 동작 방지
   isDragging = true;
   startPos = event.clientX;
-  slider.style.cursor = 'grabbing';
+  slider.style.cursor = "grabbing";
   animationID = requestAnimationFrame(animation);
 }
 
@@ -64,14 +67,17 @@ function endDrag() {
   const movedBy = currentTranslate - prevTranslate;
   const slideWidth = slider.clientWidth;
 
-  if (movedBy < -slideWidth / 10 && currentIndex < slider.childElementCount - 1) {
+  if (
+    movedBy < -slideWidth / 10 &&
+    currentIndex < slider.childElementCount - 1
+  ) {
     currentIndex += 1;
-  } else if(movedBy > slideWidth /10 && currentIndex > 0) {
+  } else if (movedBy > slideWidth / 10 && currentIndex > 0) {
     currentIndex -= 1;
   }
 
   setPositionByIndex();
-  slider.style.cursor = 'grab';
+  slider.style.cursor = "grab";
 }
 
 function setPositionByIndex() {
@@ -86,37 +92,37 @@ function animation() {
 }
 
 // 섹션바꾸기
-document.querySelector('.container').addEventListener('wheel',(event) => {
+document.querySelector(".container").addEventListener("wheel", (event) => {
   const container = event.currentTarget;
   const delta = event.deltaY;
-  const logoB = document.querySelector('.logo_b');
-  const logoY = document.querySelector('.logo_y');
-  const main = document.querySelector('.main');
-  const header = document.querySelector('.header');
+  const logoB = document.querySelector(".logo_b");
+  const logoY = document.querySelector(".logo_y");
+  const main = document.querySelector(".main");
+  const header = document.querySelector(".header");
 
   if (delta > 0) {
     container.scrollBy({
       top: window.innerHeight,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
-    logoB.style.opacity = '0';
-    logoY.style.opacity = '1';
+    logoB.style.opacity = "0";
+    logoY.style.opacity = "1";
     header.style.backgroundColor = "#ffffff";
   } else {
     container.scrollBy({
       top: -window.innerHeight,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
 
-    if (main.getBoundingClientRect().top >= -1000){
+    if (main.getBoundingClientRect().top >= -1000) {
       console.log(main.getBoundingClientRect().top);
-      logoB.style.opacity = '1';
-      logoY.style.opacity = '0';
+      logoB.style.opacity = "1";
+      logoY.style.opacity = "0";
       header.style.backgroundColor = "";
     }
   }
 
   event.preventDefault();
-})
+});
